5c1131e3e0601002c0e44a8352a8abb0412c7b72
Rin Kimhun <47348954+kimkimhun3@users.noreply.github.com>
2024-06-03 08:59:05 +0900

    Update Note.php

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Note extends Model
 {
     use HasFactory;
-
+    //to define the relation between the note and the owner
     protected $fillable = ['note', 'user_id'];
 }
